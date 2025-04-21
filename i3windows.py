ba05bf86505d896ca1265e64680f448a10280f59
Jonathan Walton <jonathan.walton@snhu.edu>
2025-04-21 16:07:46 -0700

    defined default options

--- i3windows.py
+++ i3windows.py
@@ -1,36 +1,68 @@
 #!/usr/bin/env -S python3 -u
 
-import json, subprocess, time, os, sys
+import json, subprocess
 from typing import Any
-from pprint import pprint
 from queue import Queue
 from threading import Thread
+from itertools import chain
 
 def capture(*args: str, **kwargs: Any) -> str:
 	return subprocess.check_output(args, **kwargs)
 
 if __name__ == '__main__':
-	OUTPUT = 'HDMI-0'
+	options = {
+		'source': 'HDMI-0',
+		'window': {
+			'title': {
+				'limit': 20,
+				'source': 'class'
+			},
+			'color': {
+				True: '#FFFFFF',
+				False: '#777777'
+			},
+			'count': 8
+		},
+		'separator': {
+			'color': '#777777',
+			'value': ' \u00b7 '
+		}
+	}
+
+	separator = '%{F' + options['separator']['color'] + '}' + options['separator']['value'] + '%{F-}'
+	window_label = lambda label: label if len(label) <= options['window']['title']['limit'] else label[:options['window']['title']['limit']] + '\u2026'
+	window_title = lambda window: '%{F' + options['window']['color'][window['focused']] + '}' + window_label(window['window_properties'][options['window']['title']['source']].lower()) + '%{F-}'
 
 	with subprocess.Popen(['i3-msg', '-m', '-t', 'subscribe', '["workspace", "window"]'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as listener:
 		queue = Queue()
-		thread = Thread(target = lambda p, q: [q.put(l) for l in p], args = (listener.stdout, queue), daemon = True).start()
+		Thread(target = lambda p, q: [q.put(l) for l in p], args = (listener.stdout, queue), daemon = True).start()
 
 		started = False
 		while True:
 			try:
 				if started: queue.get()
-				else:	started = True
+				else: started = True
 				focused = [node for node in json.loads(capture('i3-msg', '-t', 'get_workspaces')) if node['focused'] is True][0]
 				displays = json.loads(capture('i3-msg', '-t', 'get_tree'))['nodes']
-				display = [node for node in displays if node['name'] == OUTPUT][0]['nodes']
+				display = [node for node in displays if node['name'] == options['source']][0]['nodes']
 				workspaces = [node for node in display if node['name'] == 'content'][0]['nodes']
-				workspace = [node for node in workspaces if node['id'] == focused['id']][0]['nodes'][0]['nodes']
-				print(' %{F#777}\u00b7 '.join([
-					('%{F#fff}' if window['focused'] else '%{F#777}') +
-					window['window_properties']['class'].lower() +
-					'%{F-}'
-					for window in workspace
-				]))
+				workspace = [node for node in workspaces if node['id'] == focused['id']][0]['nodes']
+				windows = chain.from_iterable(top['nodes'] for top in workspace)
+
+				output: list[str] = [window_title(window) for window in windows]
+
+				if (len(output) > options['window']['count']):
+					output = output[:options['window']['count']]
+
+					flag = False
+					for name in output:
+						if name.startswith('%{F' + options['window']['color'][True] + '}'):
+							flag = True
+							break
+
+					if flag: output.append('%{F' + options['window']['color'][False] + '} ...%{F-}')
+					else: output.append('%{F' + options['window']['color'][True] + '} ...%{F-}')
+
+				print(separator.join(output))
 			except:
 				print('')
